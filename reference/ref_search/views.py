7c1230f8ada382e541c9139da77b83d29958097f
Tyler Inari <56182948+Tyler-Inari@users.noreply.github.com>
2020-08-01 15:44:41 +0900

    データベースと接続テスト

--- reference/ref_search/views.py
+++ reference/ref_search/views.py
@@ -1,6 +1,10 @@
 from django.views import generic
 
 # Create your views here.
-class SearchView(generic.TemplateView): #LoginRequiredMixinを先に
+class SearchView(generic.TemplateView):
     template_name = "search.html"
 search = SearchView.as_view()
+
+class ListView(generic.TemplateView):
+    template_name = "search_list.html"
+search_list = ListView.as_view()
